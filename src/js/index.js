3e83eed0ddf83c6b99faff8cab15b0c94b1957cb
Amin Hesenov <lolotron022gmail.com>
2023-09-10 07:32:30 +0400

    aded alternative algoritme and build naming

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
 const container = document.querySelector(".btnContainer");
+
+//#region main Algoritme with multi button
 let activeElements = [];
+
 const toggleActiveElement = (element) => {
     if (activeElements.includes(element)) {
         activeElements.filter(actElement => actElement !== element)
     } else activeElements.push(element)
 }
 
-container.addEventListener("click", (e) => {
+container.addEventListener("click", e => {
     const selectedElement = e.target;
     if (selectedElement.tagName === "BUTTON") {
         selectedElement.classList.toggle("activeBtn");
@@ -21,4 +24,27 @@ container.addEventListener("click", (e) => {
         }
     }
 })
+//#endregion
 
+// #region alternative algoritme
+// let activeElement;
+
+// container.addEventListener("click", e => {
+//     const selectedElement = e.target;
+//     if (selectedElement.tagName === "BUTTON") {
+//         if (selectedElement.classList.length === 3) {
+//             selectedElement.classList.add("activeBtn");
+//             if(activeElement) activeElement.classList.remove("activeBtn");
+//             activeElement = selectedElement;
+//         } else {
+//             selectedElement.classList.remove("activeBtn");
+//             activeElement = undefined;
+//         }
+//     } else {
+//         if (activeElement) {
+//             activeElement.classList.remove("activeBtn");
+//             activeElement = undefined;
+//         }
+//     }
+// })
+// #endregion
