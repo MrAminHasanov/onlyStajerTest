3ebd9b64bbff30317672f448a6fbb0b6b7f9c0ee
Amin Hesenov <lolotron022gmail.com>
2023-09-18 19:45:32 +0400

    test

--- src/js/index.js
+++ src/js/index.js
@@ -3,20 +3,12 @@ const container = document.querySelector(".btnContainer");
 //#region main Algoritme with multi button
 let activeElements = [];
 
-const toggleActiveElement = (element) => {
-    if (activeElements.includes(element)) {
-        activeElements.filter(actElement => actElement !== element)
-    } else activeElements.push(element)
-}
-
 container.addEventListener("click", e => {
     const selectedElement = e.target;
     if (selectedElement.tagName === "BUTTON") {
         selectedElement.classList.toggle("activeBtn");
-        toggleActiveElement(selectedElement);
     } else if (selectedElement.classList[0] === "structureName") {
         selectedElement.parentNode.classList.remove("activeBtn");
-        toggleActiveElement(selectedElement);
     }
     else {
         for (let element of activeElements) {
