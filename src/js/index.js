453de036bc8d78e59fd1401694bd6285cc239ec8
lolotron02 <116862198+lolotron02@users.noreply.github.com>
2023-09-18 15:45:39 +0400

    Optimaze index.js

--- src/js/index.js
+++ src/js/index.js
@@ -14,37 +14,11 @@ container.addEventListener("click", e => {
     if (selectedElement.tagName === "BUTTON") {
         selectedElement.classList.toggle("activeBtn");
         toggleActiveElement(selectedElement);
-    } else if (selectedElement.classList[0] === "structureName") {
-        selectedElement.parentNode.classList.remove("activeBtn");
-        toggleActiveElement(selectedElement);
-    }
+    } 
     else {
+        activeElements = []
         for (let element of activeElements) {
             element.classList.remove("activeBtn")
         }
     }
 })
-//#endregion
-
-// #region alternative algoritme
-// let activeElement;
-
-// container.addEventListener("click", e => {
-//     const selectedElement = e.target;
-//     if (selectedElement.tagName === "BUTTON") {
-//         if (selectedElement.classList.length === 3) {
-//             selectedElement.classList.add("activeBtn");
-//             if(activeElement) activeElement.classList.remove("activeBtn");
-//             activeElement = selectedElement;
-//         } else {
-//             selectedElement.classList.remove("activeBtn");
-//             activeElement = undefined;
-//         }
-//     } else {
-//         if (activeElement) {
-//             activeElement.classList.remove("activeBtn");
-//             activeElement = undefined;
-//         }
-//     }
-// })
-// #endregion
