932e0d2fafacb6cd4bac604da592127406b103cb
Amin Hesenov <lolotron022gmail.com>
2023-09-19 06:39:35 +0400

    latest version

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,14 @@
 const container = document.querySelector(".btnContainer");
-
-//#region main Algoritme with multi button
-let activeElements = [];
-
-const toggleActiveElement = (element) => {
-    if (activeElements.includes(element)) {
-        activeElements.filter(actElement => actElement !== element)
-    } else activeElements.push(element)
-}
+const buttons = document.getElementsByClassName("button");
 
 container.addEventListener("click", e => {
     const selectedElement = e.target;
     if (selectedElement.tagName === "BUTTON") {
         selectedElement.classList.toggle("activeBtn");
-        toggleActiveElement(selectedElement);
-    } else if (selectedElement.classList[0] === "structureName") {
-        selectedElement.parentNode.classList.remove("activeBtn");
-        toggleActiveElement(selectedElement);
     }
     else {
-        activeElements = []
-        for (let element of activeElements) {
-            element.classList.remove("activeBtn")
+        for (let button of buttons) {
+            button.classList.remove("activeBtn")
         }
     }
 })
